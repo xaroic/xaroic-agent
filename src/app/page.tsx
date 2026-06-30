57937a7851fd763269ebcd79d17442b37bc988b7
xaroic <famid473@gmail.com>
2026-06-30 14:06:33 +0700

    Create page.tsx

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import Hero from "@/components/Hero";
+import Features from "@/components/Features";
+import AgentCard from "@/components/AgentCard";
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <Features />
+      <AgentCard />
+    </>
+  );
+}
